47812d1cd434e4e416c84fbc4e9adfef196c3cbd
Paul Grever <paulgrever@gmail.com>
2015-07-29 14:37:09 -0600

    added customer testimonials

---
+++ app/assets/javascripts/testimonial.js
@@ -0,0 +1,11 @@
+// $( document ).ready(function(){
+//   myInterval;
+//   myInterval2;
+// });
+
+// var myInterval = setInterval(function () {
+//   $("#testimonial1").fadeToggle(2000);
+// },5000); 
+// var myInterval2 = setInterval(function () {
+//   $("#testimonial2").delay(5000).fadeToggle(2000);
+// },5000); 

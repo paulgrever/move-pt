88e072655f4cfb59604a7ffbec9e3f023e2f55d4
Paul Grever <paulgrever@gmail.com>
2015-08-03 15:45:47 -0600

    added figaro and maps api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :home, only: [:index]
   get "/about", to: "home#about", as: :about
   get "/blog", to: "home#blog", as: :blog
+  get "/locations", to: "home#locations", as: :locations
 end
